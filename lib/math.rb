ee825e77a7ec9a7e43496b8ba8cf3e94ba4a5c76
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-04 18:08:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,35 @@
 def addition(num1, num2)
+  puts num1 + num2
 end
+addition(1, 2)
 
 def subtraction(num1, num2)
+ puts num1 - num2
 end
+subtraction(10, 9)
 
 def division(num1, num2)
+  puts num1 / num2
 end
+division(100, 10)
 
 def multiplication(num1, num2)
+ puts num1 * num2
 end
+multiplication(20, 7)
 
 def modulo(num1, num2)
+  puts num1 % num2
 end
+modulo(9, 4)
 
 def square_root(num)
+  puts Math.sqrt(num)
 end
+square_root(81)
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  puts (num1 + num2 ( num3 )) / num4
 end
+
+order_of_operation(3, 4, 5, 2)
